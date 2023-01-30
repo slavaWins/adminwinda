c81480b7936462fb6fea6d99e44f71c6a35bac25
Вячеслав Беляков <zxc7613@mail.ru>
2023-01-30 13:46:29 +0700

    add js lib

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -20,6 +20,7 @@
 
     <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/mrpform.js') }}" defer></script>
 
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
