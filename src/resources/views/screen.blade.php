fe5f949ef3b04f4d257cff1a3bd2fddca056e55d
slavaWins <zxc7613@mail.ru>
2025-12-04 21:57:51 +0700

    menu mobile

--- src/resources/views/screen.blade.php
+++ src/resources/views/screen.blade.php
@@ -6,7 +6,56 @@
 
 @section('app-col')
 
-    <div class="col-12   adminHeader">
+    <style>
+
+        .buttonSidebarShow {
+            display: none;
+            height: 19px;
+            cursor:pointer;
+            margin-right: 11px;
+            transition: 0.2s;
+
+            @starting-style {
+                transition: 0.2s;
+                height: 2px;
+                opacity: 0;
+            }
+        }
+
+        @media (max-width: 1260px ) {
+
+            .buttonSidebarShow {
+                display: inline;
+
+            }
+
+            .sidebarFloatind {
+                display: none;
+                position: absolute !important;
+                background: var(--body-bg);
+                z-index: 1;
+
+                transition: 0.2s;
+                left:0px;
+
+                @starting-style {
+                    left:-200px;
+                    transition: 0.2s;
+                    opacity: 0;
+                }
+            }
+        }
+
+    </style>
+    <script>
+        function ClickAdminSidebarCollapce() {
+            $('.sidebarFloatind').toggle();
+        }
+    </script>
+
+
+    <div class="col-12   adminHeader" >
+        <img src="/img/menu_collapse.svg" class="buttonSidebarShow" onclick="ClickAdminSidebarCollapce()">
         @foreach(\SlavaWins\AdminWinda\Library\ParsingAdminBlade::GetAdminExtendByType("header") as $V)
             @include($V)
         @endforeach
@@ -19,7 +68,7 @@
         @include('adminwinda::sidebar')
 
 
-        <div class="col  mainContent">
+        <div class="col-md-9   mainContent">
 
             @include('adminwinda::error-alert')
 
